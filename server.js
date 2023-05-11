a51798098a2513db0effdbe49da197c7368dd68e
Reinz0n <100062773+Reinz0n@users.noreply.github.com>
2023-05-11 22:31:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "..", "app")));
+app.use(express.static(path.join(__dirname, "..", "app", "users-profile.html")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"), (err) => {
+  res.sendFile(path.join(__dirname, "users-profile.html"), (err) => {
     if (err) {
         res.status(500).send(err);
     }
